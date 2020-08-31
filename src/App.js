a1e56bf9ad203d60cdfc7fc1bdf0f52dd1f069c3
vuongvgc <vuongvgc@gmail.com>
2020-08-31 17:59:48 +0700

    status header edit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component  {
       inputText: '',
       dataEmoji: dataEmoji,
       currentPage: 1,
-      status: 'Victor' 
+      status: '' 
     }
   }
   inputChange = (event) => {
